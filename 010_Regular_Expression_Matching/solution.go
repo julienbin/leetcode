c9ea5f70d6a8c28fc9b0da911771836999d69a6b
julienbin <julienbin@gmail.com>
2017-12-19 14:12:11 +0800

    simplify the dymanic planning

--- 010_Regular_Expression_Matching/solution.go
+++ 010_Regular_Expression_Matching/solution.go
@@ -1,34 +1,19 @@
 func isMatch(s string, p string) bool {
     lens := len(s)
     lenp := len(p)
-    
-    // initial matched matrix
-    matched := make([][]bool, lenp + 1)
-    for i := 0; i <= lenp; i ++ {
-        matched[i] = make([]bool, lens + 1)
+    matched := make([][]bool, lens + 1)
+    for i := 0; i <= lens; i ++ {
+        matched[i] = make([]bool, lenp + 1)
     }
     matched[0][0] = true
-    
-    // first column
-    for i := 2; i <= lenp; i += 2 {
-        if p[i - 1] == '*' {
-            matched[i][0] = true
-        } else {
-            break
-        }
-    }
-    
-    // compute the matrix
-    for i := 1; i <= lenp; i ++ {
-        if p[i - 1] == '*' {
-            for j := 1; j <= lens; j++ {
-                matched[i][j] = matched[i - 2][j] || ((p[i - 2] == '.' || p[i - 2] == s[j - 1]) && matched[i][j - 1])
-            }        
-        } else {
-            for j := 1; j <= lens; j++ {
-                matched[i][j] = matched[i - 1][j - 1] && (p[i - 1] == '.' || p[i - 1] == s[j - 1])
+    for i := 0; i <= lens; i ++ {
+        for j := 1; j <= lenp; j ++ {
+            if j > 1 && p[j - 1] == '*' {
+                matched[i][j] = matched[i][j - 2] || (i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && matched[i - 1][j])
+            } else {
+                matched[i][j] = i > 0 && matched[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.')
             }
         }
     }
-    return matched[lenp][lens]
+    return matched[lens][lenp]
 }
